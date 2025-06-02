0592ef7bd4286a28a95568f7b4abc449e1ca5e35
Isaac <isaacmartin.dev@gmail.com>
2025-06-02 17:34:46 +0200

    FAB funcionando

--- app/src/main/java/com/isaac/ggmanager/ui/home/team/task/TaskFragment.java
+++ app/src/main/java/com/isaac/ggmanager/ui/home/team/task/TaskFragment.java
@@ -1,12 +1,15 @@
 package com.isaac.ggmanager.ui.home.team.task;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.isaac.ggmanager.databinding.FragmentTaskBinding;
+import com.isaac.ggmanager.ui.home.team.task.create.CreateTaskActivity;
 
 public class TaskFragment extends Fragment{
 
@@ -18,4 +21,14 @@ public class TaskFragment extends Fragment{
         return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setUpListeners();
+    }
+
+    private void setUpListeners() {
+        binding.fabAddTask.setOnClickListener(v -> startActivity(new Intent(getContext(), CreateTaskActivity.class)));
+    }
 }
